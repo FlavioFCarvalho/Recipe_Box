d09420ab9a2ad92a1ac98e265adbafa03a49e793
Flavio Carvalho <flavio@globalnetsis.com.br>
2016-07-21 17:44:42 -0300

    config. a rota categories

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  get 'categories/index'
+
+  get 'categories/new'
+
+  get 'categories/edit'
+
   devise_for :users
   resources :recipes
+  resources :categories
 
   root "recipes#index"
 end
