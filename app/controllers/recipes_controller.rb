3af453016ec63ef5eabd81b9126fb98b8e3b71af
FlavioFCarvalho <flaviocfr@gmail.com>
2016-07-20 21:52:41 -0300

    Add Choose a category ao simple_form do Recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -48,7 +48,7 @@ class RecipesController < ApplicationController
 	private
 
 	def recipe_params
-		params.require(:recipe).permit(:title, :description, :image, 
+		params.require(:recipe).permit(:title, :description, :image, :category_id,
 			ingredients_attributes: [:id, :name, :_destroy],
 			 directions_attributes: [:id, :step, :_destroy])
 	end
